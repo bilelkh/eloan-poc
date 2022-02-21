19e09150cece14f488172774390381946101e3c8
bilel <bilel1996k@gmail.com>
2022-02-21 16:20:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const crypto = require("crypto");
+const app = express();
+const port = 5500;
+
+const encrypt = (data) => {
+  if (!data) return data;
+  const cipher = crypto.createCipher("aes-256-cbc", "$Tl;,*/56*2F5$*88s");
+  let crypted = cipher.update(data, "utf-8", "hex");
+  crypted += cipher.final("hex");
+
+  return crypted;
+};
+
+const decrypt = (data) => {
+  if (!data) return data;
+  try {
+    const decipher = crypto.createDecipher("aes-256-cbc", "$Tl;,*/56*2F5$*88s");
+    let decrypted = decipher.update(data, "hex", "utf-8");
+    decrypted += decipher.final("utf-8");
+    return decrypted;
+  } catch (e) {
+    return data;
+  }
+};
+const msgToEncrypt = encrypt("bilel");
+const msgToDecrypt = decrypt(msgToEncrypt);
+
+console.log("msgToEncrypt", msgToEncrypt);
+console.log("msgToDecrypt", msgToDecrypt);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
